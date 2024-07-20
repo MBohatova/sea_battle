e695684e272194c1debef669afb927f8836b094e
Mariia Bohatova <maria.bogatova.01@gmail.com>
2024-07-20 23:14:24 +0300

    second board

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 let box_with_cells1 = document.querySelector('.cells_ship_box1');
+let cells_ship_box2 = document.querySelector('.cells_ship_box2');
 let num_cell_container = document.querySelector('.num_cell_container');
+let num_cell_container_2 = document.querySelector('.num_cell_container_2');
 let cell_container = document.querySelector('.cell_container');
+let cell_container_2 = document.querySelector('.cell_container_2');
 let droppable_cell_container = document.querySelector('.droppable_cell_container');
+let droppable_cell_container_2 = document.querySelector('.droppable_cell_container_2');
+let button = document.querySelector('.button');
+let button2 = document.querySelector('.button2');
+let cover_elem = document.querySelector('.cover_elem');
+let cover_elem2 = document.querySelector('.cover_elem2');
 
 let cellsWithLettersNum = 11;
 let cellsWithNumbersNum = 10;
@@ -13,6 +21,7 @@ let cell_content_nums = 0;
 let cell_content_droppable = "";
 
 let ships = document.querySelectorAll('.ship');
+let second_group_ships = document.querySelectorAll('.second_ship');
 let cells = document.querySelectorAll('.cell');
 let move_made_btn = document.querySelector('.move-made');
 let currentDroppable = null;
@@ -25,6 +34,11 @@ let id_name = "";
 let id_arr = [];
 let elem_id = "";
 
+let boardX = droppable_cell_container.getBoundingClientRect().left;
+let boardY = droppable_cell_container.getBoundingClientRect().top;
+let second_boardX = droppable_cell_container_2.getBoundingClientRect().left;
+let second_boardY = droppable_cell_container_2.getBoundingClientRect().top;
+
 for(let i = 0; i < cellsWithLettersNum; i++) {
   if(i > 0) {
     cell_content_letters = letters[i - 1];
@@ -34,17 +48,22 @@ for(let i = 0; i < cellsWithLettersNum; i++) {
     cell_content_letters = "";
   }
   cell_container.insertAdjacentHTML('beforeEnd', `<div class="cell">${cell_content_letters}</div>`);
+  cell_container_2.insertAdjacentHTML('beforeEnd', `<div class="cell">${cell_content_letters}</div>`);
 }
 
 for(let i = 0; i < cellsWithNumbersNum; i++) {
   cell_content_nums ++;
   num_cell_container.insertAdjacentHTML('beforeEnd', `<div class="cell">${cell_content_nums}</div>`);
+  num_cell_container_2.insertAdjacentHTML('beforeEnd', `<div class="cell">${cell_content_nums}</div>`);
 }
 
 for(let i = 0; i < droppableCells; i++) {
   id_name = i + '_cell';
   id_arr.push(id_name);
   droppable_cell_container.insertAdjacentHTML('beforeEnd', `<div id="${id_name}" class="cell droppable">${cell_content_droppable}</div>`);
+  droppable_cell_container_2.insertAdjacentHTML('beforeEnd', `<div id="${id_name}" class="cell second_droppable">${cell_content_droppable}</div>`);
+  cover_elem.insertAdjacentHTML('beforeEnd', `<div id="${id_name}" class="cell invalid_cell">${cell_content_droppable}</div>`);
+  cover_elem2.insertAdjacentHTML('beforeEnd', `<div id="${id_name}" class="cell invalid_cell">${cell_content_droppable}</div>`);
 }
 
 for(let ship of ships) {
@@ -70,6 +89,7 @@ for(let ship of ships) {
 
       ship.hidden = true;
       let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+      console.log(elemBelow);
       ship.hidden = false;
 
       if(!elemBelow) return;
@@ -77,24 +97,16 @@ for(let ship of ships) {
       let droppableBelow = elemBelow.closest('.droppable');
 
       if(currentDroppable != droppableBelow) {
-        if(currentDroppable) {
-          leaveDroppable(currentDroppable);
-        }
-
         currentDroppable = droppableBelow;
-        if(currentDroppable) {
-          enterDroppable(currentDroppable);
-
-          let boardX = droppable_cell_container.getBoundingClientRect().left;
-          let boardY = droppable_cell_container.getBoundingClientRect().top;
-      
-          function getShipCoordinate(ship_coordinate, board_coordinate, shift) {
-            return (Math.floor((ship_coordinate - board_coordinate - shift) / sizeOfCell) * sizeOfCell);
-          }
+        if(currentDroppable) { 
           leftShipCoordinate = getShipCoordinate(event.pageX, boardX, shiftX);
-          topShipCoordinate = getShipCoordinate(event.pageY, boardY, shiftY);
+          topShipCoordinate = getShipCoordinate(event.pageY, boardY, shiftY) + sizeOfCell;
+
+          console.log(leftShipCoordinate, 'left');
+          console.log(topShipCoordinate, 'top');
         
           elem_id = droppableBelow.getAttribute('id');
+          console.log(elem_id);
         }
       }
     }
@@ -104,9 +116,6 @@ for(let ship of ships) {
     ship.onmouseup = function() {
       document.removeEventListener('mousemove', onMouseMove);
       ship.onmouseup = null;
-
-      let boardX = droppable_cell_container.getBoundingClientRect().left;
-      let boardY = droppable_cell_container.getBoundingClientRect().top;
 
       maxAvailableCellX = (cellsWithNumbersNum - (ship.getBoundingClientRect().width / sizeOfCell)) * sizeOfCell;
 
@@ -123,30 +132,8 @@ for(let ship of ships) {
 
       ship.style.left = leftShipCoordinate + boardX + 'px';
       ship.style.top = topShipCoordinate + boardY + 'px';
-
-      for(let i = 0; i < id_arr.length; i++) {
-        if(id_arr[i] === elem_id) {
-          document.getElementById(id_arr[i + 1]).style.backgroundColor = 'red';
-          document.getElementById(id_arr[i - 1]).style.backgroundColor = 'red';
-          document.getElementById(id_arr[i + 10]).style.backgroundColor = 'red';
-          document.getElementById(id_arr[i - 10]).style.backgroundColor = 'red';
-          document.getElementById(id_arr[i - 11]).style.backgroundColor = 'red';
-          document.getElementById(id_arr[i - 9]).style.backgroundColor = 'red';
-          document.getElementById(id_arr[i + 9]).style.backgroundColor = 'red';
-          document.getElementById(id_arr[i + 11]).style.backgroundColor = 'red';
-        }
-      }
     }
-  }
-
-  function enterDroppable(elem) {
-    if(elem.textContent !== '') {
-      elem.style.background = '';
-    }
-  }
-
-  function leaveDroppable(elem) {
-    elem.style.background = '';
+    moveMade(button, ship, cover_elem);
   }
 
   ship.ondragstart = function() {
@@ -154,6 +141,88 @@ for(let ship of ships) {
   };
 }
 
-// координати борду (відносно сторінки) - координати мишки (відносно сторінки) = координати мишки (відносно борду)
-// якщо координати мишки (відносно борду) > 0 та < 20 (гіпотетичний розмір клітинки), тоді координати мишки мають стати 0.
-// координати мишки (відносно борду) / на розмір одної клітинки і округлюю до цілого меншого. 
+for(let second_ship of second_group_ships) {
+  second_ship.onmousedown = function(event) {
+
+    let shiftX = event.clientX - second_ship.getBoundingClientRect().left;
+    let shiftY = event.clientY - second_ship.getBoundingClientRect().top;
+  
+      second_ship.style.position = 'absolute';
+      second_ship.style.zIndex = 1;
+      document.body.appendChild(second_ship);
+  
+      moveAt(event.pageX, event.pageY);
+  
+      function moveAt(pageX, pageY) {
+        second_ship.style.left = pageX - shiftX + 'px';
+        second_ship.style.top = pageY - shiftY + 'px';
+      }
+  
+      function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+  
+        second_ship.hidden = true;
+        let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+        second_ship.hidden = false;
+  
+        if(!elemBelow) return;
+  
+        let droppableBelow = elemBelow.closest('.second_droppable');
+  
+        if(currentDroppable != droppableBelow) {
+          currentDroppable = droppableBelow;
+          if(currentDroppable) { 
+            leftShipCoordinate = getShipCoordinate(event.pageX, second_boardX, shiftX);
+            topShipCoordinate = getShipCoordinate(event.pageY, second_boardY, shiftY) + sizeOfCell;
+  
+            console.log(leftShipCoordinate, 'left');
+            console.log(topShipCoordinate, 'top');
+          
+            elem_id = droppableBelow.getAttribute('id');
+            console.log(elem_id);
+          }
+        }
+      }
+  
+      document.addEventListener('mousemove', onMouseMove);
+
+      second_ship.onmouseup = function() {
+        document.removeEventListener('mousemove', onMouseMove);
+        second_ship.onmouseup = null;
+  
+        maxAvailableCellX = (cellsWithNumbersNum - (second_ship.getBoundingClientRect().width / sizeOfCell)) * sizeOfCell;
+  
+        if(leftShipCoordinate > maxAvailableCellX) {
+          leftShipCoordinate = maxAvailableCellX;
+        }
+  
+        if(leftShipCoordinate < 0) {
+          leftShipCoordinate = 0;
+        }
+        if(topShipCoordinate < 0) {
+          topShipCoordinate = 0;
+        }
+  
+        second_ship.style.left = leftShipCoordinate + second_boardX + 'px';
+        second_ship.style.top = topShipCoordinate + second_boardY + 'px';
+      }
+
+    moveMade(button2, second_ship, cover_elem2);
+  }
+  
+  second_ship.ondragstart = function() {
+      return false;
+  };
+}
+
+function getShipCoordinate(ship_coordinate, board_coordinate, shift) {
+  return (Math.floor((ship_coordinate - board_coordinate - shift) / sizeOfCell) * sizeOfCell);
+}
+
+function moveMade(btn, elem, cover) {
+  btn.addEventListener('click', function() {
+    btn.style.backgroundColor = 'darkBlue';
+    elem.onmousedown = null;
+    cover.style.display = 'inherit';
+  });
+};
